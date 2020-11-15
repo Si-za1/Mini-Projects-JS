d2133d20a954ff10ff7231d44b89f648c77959ae
Siza <siza2459@gmail.com>
2020-11-15 20:00:06 +0545

    bg change onclick

--- Background-Change/index.js
+++ Background-Change/index.js
@@ -1,9 +1,11 @@
 function change()
 {
-    randomColors=['red','cyan','pink','lime','blue','orange'];
+    randomColors=['red','cyan','pink','lime','blue','orange']; //Making the array which is the collection of the colors name 
 
-    var exchange= randomColors[Math.floor(Math.random() * randomColors.length)];
+    var exchange= randomColors[Math.floor(Math.random() * randomColors.length)]; 
+    //To access the colors calling the floor function where the digits will be converted into the integer and random numbers will be generated within the 
+    // randomcolors(array)'s length
 
-    document.body.style.background=exchange;
+    document.body.style.background=exchange; //accessing the document where the css property bg is called and is linked with the var exchnage 
 
 }
